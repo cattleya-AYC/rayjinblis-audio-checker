b9bab0866dc64707cc332e6a8ef6e6e8897b3574
cattleya-AYC <rayjinblis@gmail.com>
2026-06-03 20:43:54 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './',
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
+  root: './src',
+  build: {
+    outDir: '../dist'
   }
 })
